fadb6d4b95cc821eeffb04cb329ca28b2b83bef6
juan759 <jasso.antonio2001@gmail.com>
2021-01-06 16:10:42 -0600

    State medio completo.Cambiados metodos del Diagrama.

--- EstadoNoche.java
+++ EstadoNoche.java
@@ -1,6 +1,6 @@
 
 public class EstadoNoche implements EstadoAldea{
-    
+
     protected Aldea narrador;
 
     /**
@@ -13,27 +13,30 @@ public class EstadoNoche implements EstadoAldea{
 
     @Override
     public void convocarJuicio(){
-
+        System.out.println("No es posible convocar al Juicio comunal");
+        throw new UnsupportedOperationException("Accion no disponible para la aldea");
     }
 
     @Override
     public void recuentoNoche(){
-
+        System.out.println("Aún no hay muertos.");
+        throw new UnsupportedOperationException("Accion no disponible para la aldea");
     }
 
     @Override
     public void convocarBanquete(){
-
+        System.out.println("Hombres-Lobo transformandose para comer.");
     }
 
     @Override
     public void anochece(){
-
+        System.out.println("Ya es de noche.");
+        throw new UnsupportedOperationException("Accion no disponible para la aldea");
     }
 
     @Override
     public void amanece(){
-
+        System.out.println("Esta amaneciendo.");
     }
 
 }
