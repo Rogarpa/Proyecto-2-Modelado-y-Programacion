60e3a044fe082e8217f4edd67be9b60196a7e72d
Rodrigo García <rodrigogarciapadilla@ciencias.unam.mx>
2021-01-17 19:43:47 -0600

    Primer compilacion v1

--- main.java
+++ main.java
@@ -1,33 +1,10 @@
 
 public class main{
-        public static void main(String[] args){
 
-        /**AldeanoComun uno=new AldeanoComun();
-        Vidente moni=Vidente.getinstance();
-        Cazador dos=Cazador.getinstance();
-        Caballero tres=Caballero.getinstance();
-        Lobo cuatro=new Lobo();
-        Angel cinco=Angel.getinstance();
-
-        Magia magia=new MagiaCazador(tres,primera);
-
-        cuatro.clone();
-        tres.setRehen(cinco);
-        uno.getArma();
-        moni.getRopa();
-        dos.getCabello();
-        tres.getId();
-        cuatro.getRopa();
-        uno.getCabello();
-        moni.getArma();
-        dos.getCabello();
-        tres.getArma();
-        cuatro.getArma();
-        System.out.println(tres.getRehen());
-
-        primera.matar(dos,cuatro);*/    
-
-
-
+    public static void main(String[] args){
+        MenusJuego menu = new MenusJuego();
+        ControladorJuego controladorAldea = null;
+        Aldea aldea1 = new Aldea(controladorAldea);
+        controladorAldea = new ControladorJuego(aldea1, menu);
     }
 }
